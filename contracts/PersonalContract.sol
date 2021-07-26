dc2aa411e89af5e64a948fb94b48d4023255000f
Surya Bakshi <surya.bakshi@protonmail.com>
2021-07-26 15:53:46 -0400

    basic functionality compiled, not tested

--- contracts/PersonalContract.sol
+++ contracts/PersonalContract.sol
@@ -11,9 +11,13 @@ contract PersonalContract is IPersonalContract {
         uint256 eventHash;
         address who;
         uint256 epoch_timestamp;
+        bool cancelled;
+        string reason;
         // TODO: maybe something for "where" either physical or virtual? (privacy)
     }
 
+
+    mapping (address => uint256) balances;
     mapping (uint256 => Event) events;
 
 	modifier onlyOwner() {
@@ -21,19 +25,30 @@ contract PersonalContract is IPersonalContract {
 		_;
 	}
 
+    event NewEvent(uint256 indexed eventHash, address indexed fro, uint256 timestamp);
+    event Cancelled(uint256 indexed eventHash, address indexed fro, string reason);
+
 	constructor (string memory _name) public {
 		name = _name;
 	}
 	
 	
     function redeemCard(bytes32 hash, uint8 v, bytes32 r, bytes32 s) external override returns (bool) {
-        // TODO implementation
+        require( ECDSA.recover(hash, v, r, s) == owner );
+        balances[msg.sender] += 1;
         return true;
     }
     
     // Redeems one hour slot for a specific timestamp
     function scheduleHour(uint256 time_since_epoch) external override returns (bool) {
-        // TODO implementation
+        require(balances[msg.sender] >= 1);
+        require(block.timestamp <= time_since_epoch);
+        balances[msg.sender] -= 1;
+        
+        uint256 key = uint256(uint160(address(msg.sender)))  ^ time_since_epoch;
+        events[key] = Event(key, msg.sender, time_since_epoch, false, "");
+        
+        emit NewEvent(key, msg.sender, time_since_epoch);
         return true;
     }
     
@@ -42,7 +57,15 @@ contract PersonalContract is IPersonalContract {
     
     // decline the communication with a string reason why and return of spent "token" (? is token the right word)?
     function declineHour(uint256 eventHash, string memory reason) external override {
-        // TODO implementation
+        Event storage e = events[eventHash];
+        require( events[eventHash].who != address(0) );
+        require( e.cancelled == false);
+        require(block.timestamp >= e.epoch_timestamp);
+        
+        e.cancelled = true;
+        e.reason = reason;
+        
+        emit Cancelled(e.eventHash, e.who, e.reason);
     }
     
     // decline and remove the user's allowed schedule slots
